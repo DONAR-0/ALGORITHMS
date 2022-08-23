e84f4668789cd6593b97a3a324410aea4d215cb9
DONAR-0 <himalaya.kumar601@outlook.com>
2022-08-23 23:26:52 +0530

    Common Programs

---
+++ awsome/src/main/java/aw/some/Scenario14.java
@@ -0,0 +1,25 @@
+package aw.some;
+
+import java.util.Arrays;
+
+/**
+* Scenario14
+*/
+public class Scenario14 {
+
+	public int averageValueOfArray(int[] array) {
+		
+		var sum = 0;
+		for (int i : array) {
+	             	sum = sum + i;
+		}
+		return sum / (array.length - 1);
+	}
+
+	public Integer averageValueOfArray1(Integer[] array){
+		return Arrays.asList(array)
+		.stream()
+		.mapToInt(i -> i)
+		.sum() / (array.length - 1);
+	}
+}
