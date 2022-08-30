9044d7f4a87bc9aee0df8e649f215d56ef3e6dec
DONAR-0 <himalaya.kumar@outlook.com>
2022-08-30 22:45:35 +0530

    Merge Linked List Iterative way

--- awsome/src/main/java/aw/some/Scenario18.java
+++ awsome/src/main/java/aw/some/Scenario18.java
@@ -1,8 +1,9 @@
 package aw.some;
 
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringJoiner;
 
-import lombok.val;
 
 /**
 * Scenario18
@@ -10,13 +11,59 @@ import lombok.val;
 public class Scenario18 {
 
 	/**
-	*
+	* Merge Two Sorted list
 	*
 	* */
 	public ListNode mergeTwoList(ListNode listNode1,ListNode listNode2) {
-		return null;
+		if(listNode1 == null) {
+			return listNode2;
+		} 
+		if(listNode2 == null) {
+			return listNode1;
+		}
+		ListNode newList = null;
+		while(listNode1 != null && listNode2 != null) {
+			if(listNode1.val > listNode2.val) {
+				newList = addLast(newList,listNode2.val);
+				listNode2 = listNode2.next;
+			} else {
+				newList = addLast(newList,listNode1.val);
+				listNode1 = listNode1.next;
+			}	
+		}
+		while(listNode1 != null) {
+			newList = addLast(newList,listNode1.val);
+			listNode1 = listNode1.next;
+		}
+		while(listNode2 != null) {
+			newList = addLast(newList,listNode2.val);
+			listNode2 = listNode2.next;
+		}
+		return newList;
 	}
 
+	/**
+	* Add last
+	* */
+	public ListNode addLast(ListNode head,int val){
+		ListNode current = head;
+		ListNode newNode = new ListNode(val);
+		if(current == null) {
+			head = newNode;
+			return head;
+		}
+		while (current.next != null) {
+			current = current.next;
+		}
+		current.next = newNode;
+		return newNode;
+	}
+
+	/**
+	* print Linked list 
+	* @param head
+	*
+	* */
 	public String printList(ListNode head){
 		ListNode currentNode = head;
 		StringJoiner stringJoiner = new StringJoiner(" -> ");
--- awsome/src/test/java/aw/some/Scenario18Tests.java
+++ awsome/src/test/java/aw/some/Scenario18Tests.java
@@ -15,7 +15,7 @@ public class Scenario18Tests {
 
 	@Test
 	@DisplayName("scenario18_Test_01")
-	public void Scenario18_Test_01() {
+	public void scenario18_Test_01() {
 		Scenario18 scenario18 = new Scenario18();
 		Scenario18.ListNode next4 = new Scenario18.ListNode(4);
 		Scenario18.ListNode next3 = new Scenario18.ListNode(4,next4);
@@ -24,4 +24,38 @@ public class Scenario18Tests {
 		Scenario18.ListNode head = new Scenario18.ListNode(1,next);
 		LOG.info("{}",scenario18.printList(head));
 	}
+
+	@Test
+	@DisplayName("scenario18_Test_02")
+	public void scenario18_Test_02() {
+		Scenario18 scenario18 = new Scenario18();
+		Scenario18.ListNode next4 = new Scenario18.ListNode(5);
+		Scenario18.ListNode next3 = new Scenario18.ListNode(4,next4);
+		Scenario18.ListNode next2 = new Scenario18.ListNode(3,next3);
+		Scenario18.ListNode next = new Scenario18.ListNode(2,next2);
+		Scenario18.ListNode head = new Scenario18.ListNode(1,next);
+
+		Scenario18.ListNode next4_2 = new Scenario18.ListNode(5);
+		Scenario18.ListNode next3_2 = new Scenario18.ListNode(4,next4_2);
+		Scenario18.ListNode next2_2 = new Scenario18.ListNode(3,next3_2);
+		Scenario18.ListNode next_2 = new Scenario18.ListNode(2,next2_2);
+		Scenario18.ListNode head_2 = new Scenario18.ListNode(1,next_2);
+		
+		Scenario18.ListNode merged_head = scenario18.mergeTwoList(head, head_2);
+		LOG.info("{}",scenario18.printList(merged_head));
+	}
+	
+	@Test
+	@DisplayName("scenario18_Test_03")
+	public void scenario18_Test_03() {
+		Scenario18 scenario18 = new Scenario18();
+		Scenario18.ListNode next4 = new Scenario18.ListNode(5);
+		Scenario18.ListNode next3 = new Scenario18.ListNode(4,next4);
+		Scenario18.ListNode next2 = new Scenario18.ListNode(3,next3);
+		Scenario18.ListNode next = new Scenario18.ListNode(2,next2);
+		Scenario18.ListNode head = new Scenario18.ListNode(1,next);
+		scenario18.addLast(head,6);
+		LOG.info("{}",scenario18.printList(head));
+	}
+
 }
