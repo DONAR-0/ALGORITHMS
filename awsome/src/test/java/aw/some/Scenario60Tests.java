0212820c4191916d9def8ec44ef8764aa13e339c
DONAR-0 <himalaya.kumar601@outlook.com>
2023-01-18 21:46:33 +0530

    idea switch

--- awsome/src/test/java/aw/some/Scenario60Tests.java
+++ awsome/src/test/java/aw/some/Scenario60Tests.java
@@ -1,4 +1,20 @@
 package aw.some;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@DisplayName("Number of sub")
 public class Scenario60Tests {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(Scenario60Tests.class);
+
+	@Test
+	@DisplayName("TEST_1")
+	public void testNumberOfSubsets(){
+		int[] arr = new int[]{12, 13, 21, 25, 26, 9, 6, 11};
+		LOGGER.info("{}",Scenario60.subsetThatSumsUptoK(arr, 25));
+	}
 }
