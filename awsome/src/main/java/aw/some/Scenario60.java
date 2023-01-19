903159100467a4d032ff7c67334df6fc1debadf3
himalaya-kumar <itshimalaykumarx@gmail.com>
2023-01-19 21:12:38 +0530

    Working Scenario 60

--- awsome/src/main/java/aw/some/Scenario60.java
+++ awsome/src/main/java/aw/some/Scenario60.java
@@ -5,25 +5,25 @@ package aw.some;
  */
 public class Scenario60 {
 
-    public static int subsetThatSumsUptoK(int[] arr,int k,int i,int sum) {
-        if (sum== k) {
+    public static int subsetThatSumsUptoK(int[] arr, int k, int i, int sum) {
+        if (sum == k) {
             return 1;
         } else if (sum > k || i >= arr.length) {
             return 0;
         } else
-            return subsetThatSumsUptoK(arr, k, i + 1, sum + arr[i]) + subsetThatSumsUptoK(arr,k,i + 1,sum);
+            return subsetThatSumsUptoK(arr, k, i + 1, sum + arr[i]) + subsetThatSumsUptoK(arr, k, i + 1, sum);
     }
 
     /**
      * By Using recursion
-     * TC: O( 2 ^n)
+     * TC: O( 2 ^n )
      * SC: O(n)
      *
      * @param arr array
-     * @param k value add upto
+     * @param k   value add upto
      * @return int value
      */
-    public static int subsetThatSumsUptoK(int[] arr,int k) {
-        return subsetThatSumsUptoK(arr,k,0,0);
+    public static int subsetThatSumsUptoK(int[] arr, int k) {
+        return subsetThatSumsUptoK(arr, k, 0, 0);
     }
 }
--- awsome/src/test/java/aw/some/Scenario60Tests.java
+++ awsome/src/test/java/aw/some/Scenario60Tests.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.Is.is;
+
 
 @DisplayName("Number of sub")
 public class Scenario60Tests {
@@ -16,5 +20,6 @@ public class Scenario60Tests {
 	public void testNumberOfSubsets(){
 		int[] arr = new int[]{12, 13, 21, 25, 26, 9, 6, 11};
 		LOGGER.info("{}",Scenario60.subsetThatSumsUptoK(arr, 25));
+		assertThat(Scenario60.subsetThatSumsUptoK(arr,25),is(equalTo(2)));
 	}
 }
