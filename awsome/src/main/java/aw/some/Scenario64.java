c38e1c7a9cfc367ae7ffaf50f645420124772c4f
DONAR-0 <108885663+DONAR-0@users.noreply.github.com>
2023-01-26 13:53:10 +0530

    Scenario64 (#2)
    * Added else if
    
    * making many changes as i like
    
    * Make as many commit as i like
    
    * Scenario64-4 Done make many changes as i can make
    
    Co-authored-by: himalaya-kumar <itshimalaykumarx@gmail.com>

--- awsome/src/main/java/aw/some/Scenario64.java
+++ awsome/src/main/java/aw/some/Scenario64.java
@@ -13,39 +13,39 @@ public class Scenario64 {
      * TC: O((n+m).2^n)
      * SC: O(n.2^n)
      */
-    public static boolean isSubSequence(String string1,String string2) {
-        int ptr1 = 0,ptr2 = 0;
+    public static boolean isSubSequence(String string1, String string2) {
+        int ptr1 = 0, ptr2 = 0;
         while (ptr1 < string1.length() && ptr2 < string2.length()) {
             if (string1.charAt(ptr1) == string2.charAt(ptr2)) {
-                ptr1 ++;
-                ptr2 ++;
+                ptr1++;
+                ptr2++;
             } else {
-                ptr1 ++;
+                ptr1++;
             }
         }
         return ptr2 == string2.length();
     }
 
-    public static void getSubsequences(String str, int i, String subsequence, ArrayList<String> subsequences){
+    public static void getSubsequences(String str, int i, String subsequence, ArrayList<String> subsequences) {
         if (i == str.length()) {
             subsequences.add(subsequence);
         } else {
-            getSubsequences(str, i + 1,subsequence + str.charAt(i), subsequences);
-            getSubsequences(str, i + 1,subsequence, subsequences);
+            getSubsequences(str, i + 1, subsequence + str.charAt(i), subsequences);
+            getSubsequences(str, i + 1, subsequence, subsequences);
         }
     }
 
     public static ArrayList<String> getSubsequences(String str) {
         ArrayList<String> subsequences = new ArrayList<>();
-        getSubsequences(str,0,"",subsequences);
+        getSubsequences(str, 0, "", subsequences);
         return subsequences;
     }
 
-    public static int lcs_1(String str1,String str2) {
+    public static int lcs_1(String str1, String str2) {
         int maxLength = 0;
         ArrayList<String> subsequences = getSubsequences(str1);
-        for (String subsequence: subsequences) {
-            if (isSubSequence(str2,subsequence) && subsequence.length() > maxLength) {
+        for (String subsequence : subsequences) {
+            if (isSubSequence(str2, subsequence) && subsequence.length() > maxLength) {
                 maxLength = subsequence.length();
             }
         }
@@ -56,33 +56,66 @@ public class Scenario64 {
      * Recursive solution:
      * Time Complexity: O(2^(m+n))
      * Space Complexity: O(n + m)
-     *
      */
-    public static int lcs_2(String str1,String str2,int i,int j) {
+    public static int lcs_2(String str1, String str2, int i, int j) {
         if (i == str1.length() || j == str2.length()) {
             return 0;
-        }
-        else if (str1.charAt(i) == str2.charAt(j)) {
-            return 1 + lcs_2(str1,str2,i + 1,j +1);
+        } else if (str1.charAt(i) == str2.charAt(j)) {
+            return 1 + lcs_2(str1, str2, i + 1, j + 1);
         } else {
-            return Math.max(lcs_2(str1, str2, i + 1, j),lcs_2(str1, str2, i, j + 1));
+            return Math.max(lcs_2(str1, str2, i + 1, j), lcs_2(str1, str2, i, j + 1));
         }
     }
 
-    public static int lcs_2(String str1,String str2) {
-        return lcs_2(str1,str2,0,0);
+    public static int lcs_2(String str1, String str2) {
+        return lcs_2(str1, str2, 0, 0);
     }
 
     // Time-optimized recursive solution(memoization):
     // Time Complexity: O(nm)
     // Space Complexity: O(nm)
-    public static int lcs_3(String str1, String str2, int i, int j, HashMap<String,Integer> memoiz) {
+    public static int lcs_3(String str1, String str2, int i, int j, HashMap<String, Integer> memoiz) {
         String key = i + " " + j;
-        if(memoiz.containsKey(key)) {
+        if (memoiz.containsKey(key)) {
             return memoiz.get(key);
         } else if (i == str1.length() || j == str2.length()) {
             return 0;
+        } else if (str1.charAt(i) == str2.charAt(j)) {
+            return 1 + lcs_3(str1, str2, i + 1, j + 1, memoiz);
+        } else {
+            int output = Math.max(lcs_3(str1, str2, i + 1, j, memoiz), lcs_3(str1, str2, i, j + 1, memoiz));
+            memoiz.put(key, output);
+            return output;
         }
-        return i;
+    }
+
+    public static int lcs_3(String str1, String str2) {
+        HashMap<String, Integer> memoiz = new HashMap<>();
+        return lcs_3(str1, str2, 0, 0, memoiz);
+    }
+
+    /**
+     * Dynamic Programming solution:
+     * Time Complexity: O(nm)
+     * Space Complexity: O(mn)
+     *
+     * @param str1
+     * @param str2
+     * @return
+     */
+    public static int lcs_4(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+        int[][] dp = new int[n + 1][ m + 1];
+        for (int i = 1; i < n +1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[n][m];
     }
 }
--- awsome/src/test/java/aw/some/Scenario64Tests.java
+++ awsome/src/test/java/aw/some/Scenario64Tests.java
@@ -24,4 +24,17 @@ public class Scenario64Tests {
     public void testScenarioLogestCommonSubsequences_2(){
         LOGGER.info("{}",Scenario64.lcs_2("longcommonsub","common"));
     }
+
+
+    @Test
+    @DisplayName("TEST_3")
+    public void testScenarioLogestCommonSubsequences_3(){
+        LOGGER.info("{}",Scenario64.lcs_3("longcommonsub","common"));
+    }
+
+    @Test
+    @DisplayName("TEST_4")
+    public void testScenarioLogestCommonSubsequences_4(){
+        LOGGER.info("{}",Scenario64.lcs_4("longcommonsub","common"));
+    }
 }
