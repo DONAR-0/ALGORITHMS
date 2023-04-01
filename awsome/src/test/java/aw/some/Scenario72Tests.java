fb7b261aac16246049359c2371533eac110f05d4
himalaya-kumar <itshimalaykumarx@gmail.com>
2023-04-01 20:43:22 +0530

    update

--- awsome/src/test/java/aw/some/Scenario72Tests.java
+++ awsome/src/test/java/aw/some/Scenario72Tests.java
@@ -1,7 +1,7 @@
 package aw.some;
 
-import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
