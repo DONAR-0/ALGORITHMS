cce13e76d1ec8cf12de149ca8b47f5ccdbe2b4af
himalaya-kumar <itshimalaykumarx@gmail.com>
2023-04-30 21:33:53 +0530

    scenario 4

--- awsome/src/main/java/v_4/Sc_4.java
+++ awsome/src/main/java/v_4/Sc_4.java
@@ -3,6 +3,7 @@ package v_4;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 /**
  * Finding duplicates
@@ -46,7 +47,26 @@ public class Sc_4 {
         return 0;
     }
 
-    public static void main(String[] args) {
+    /**
+     * Finding duplicates by using the dictionay
+     * TC: O(n)
+     * SPC: O(n)
+     *
+     * @param arr
+     */
+    public static int findDuplicates_3(int[] arr) {
+        HashMap<Integer,Boolean> visited = new HashMap<>();
+        for (int element: arr) {
+            if (visited.containsKey(element)) return element;
+            else visited.put(element,true);
+        }
+        return 0;
+    }
 
+    public static void main(String[] args) {
+        int[] arr = new int[]{12,13,14,15,16,23,25,15};
+        log.info("{}",Sc_4.findDuplicates(arr));
+        log.info("{}",Sc_4.findDuplicates_2(arr));
+        log.info("{}",Sc_4.findDuplicates_3(arr));
     }
 }
